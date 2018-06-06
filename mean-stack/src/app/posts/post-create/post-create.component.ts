231d1d47ff68b7747a9ce0e539ee11c2519346b0
Patrick Ly <patrickkimly@gmail.com>
2018-06-06 13:36:50 -0700

    Sec 2  Creating a Post Model

--- mean-stack/src/app/posts/post-create/post-create.component.ts
+++ mean-stack/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Post } from '../post-model';
 
 @Component({
   selector: "app-post-create",
@@ -9,11 +10,11 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class PostCreateComponent {
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
   newPost = 'No Content';
 
   onAddPost() {
-    const post = {
+    const post: Post = {
       title: this.enteredTitle,
       content: this.enteredContent
     };
