3a34073913cf3620be50a44a2c9fad57070e3fe9
patrickly <patrickkimly@gmail.com>
2018-06-29 23:53:42 -0700

    Sec 4, Lec 56 Updating the Frontend after deleting posts

--- mean-stack/src/app/posts/posts.service.ts
+++ mean-stack/src/app/posts/posts.service.ts
@@ -5,27 +5,27 @@ import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: any }>(
-        "http://localhost:3000/api/posts"
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map(postData => {
+          return postData.posts.map(post => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id
+            };
+          });
+        })
       )
-      .pipe(map((postData) => {
-        return postData.posts.map(post => {
-          return {
-            title: post.title,
-            content: post.content,
-            id: post._id
-          };
-        });
-      }))
       .subscribe(transformedPosts => {
         this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
@@ -39,7 +39,7 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe(responseData => {
         console.log(responseData.message);
         this.posts.push(post);
@@ -48,10 +48,12 @@ export class PostsService {
   }
 
   deletePost(postId: string) {
-    this.http.delete("http://localhost:3000/api/posts/" + postId)
+    this.http
+      .delete('http://localhost:3000/api/posts/' + postId)
       .subscribe(() => {
-        console.log("Deleted");
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
   }
-
 }
